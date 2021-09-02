0e48110580d5cc7ae64b64876c0277bc4b225c47
nioosha67 <88873759+nioosha67@users.noreply.github.com>
2021-09-02 11:25:42 +0430

    Add files via upload

---
+++ s3h2.py
@@ -0,0 +1,13 @@
+#Nioosha Asadi Moghadas-Thursday 14-18
+#tabei ba 3 voroodi, 2 adad va 1 amalgar, mohasebe 2 adad bar asase amalgar
+
+def mohasebe(adad1,adad2,amalgar):
+    if(amalgar=='+'):
+        print(adad1+adad2)
+    elif(amalgar=='-'):
+        print(adad1-adad2)
+    elif(amalgar=='*'):
+        print(adad1*adad2)
+    else:
+        print(adad1/adad2)
+mohasebe(100,2,'/')
